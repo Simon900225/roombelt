7e535b8a1fb707edff2edba5fbdee1c37e3e3360
Simon Bengtsson <simon.bengtsson@goodsolutions.se>
2018-11-26 12:17:44 +0100

    ButtonSet is now reactive to orientation

--- frontend/src/apps/device/display/actions-bar/components/ButtonSet.js
+++ frontend/src/apps/device/display/actions-bar/components/ButtonSet.js
@@ -2,10 +2,21 @@ import styled from "styled-components/macro";
 import { Button } from "theme/index";
 
 export default styled.div`
-  display: inline-block;
+  @media screen and (orientation:landscape){
+    display: inline-block;  
+  }
 
+  @media screen and (orientation:portrait){
+    display: flex;
+    width: auto;
+    flex-wrap: wrap;
+  }
+  
   & ${Button} {
     border-radius: 0;
+    @media screen and (orientation:portrait){
+      min-width:50%;
+    }
   }
 
   & ${Button} + ${Button} {
